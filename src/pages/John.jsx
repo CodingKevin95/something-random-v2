a7b4f239d80a3fc21af6d6674a631fca1fe752dd
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-08-02 18:47:44 -0400

    resolved issue 14

--- src/pages/John.jsx
+++ src/pages/John.jsx
@@ -1,9 +1,6 @@
 import "./John.css"
 import audio from "../assets/skullsound2.mp3"
 function John(){
-const container = document.createElement('img')
-container.src = "https://media3.giphy.com/media/J0WeVOLjuqW2I/giphy.gif"
-
 
 function handelCLick(){
     const container = document.createElement('img')
@@ -11,43 +8,30 @@ function handelCLick(){
     container.src = "https://media3.giphy.com/media/J0WeVOLjuqW2I/giphy.gif"
     const sound = new Audio(audio);
     sound.play()
-
-     
-    
-return(
-    document.body.appendChild(container)
-    )
+    document.getElementById("john-container").appendChild(container);
 }
 
     return(
-        <div className="JohnContainer">
-          <h1>Johns Harrison</h1>
-          <h3>"Professional Stack Overflow Explorer"</h3>
-          <div>
-            <div className="Container">
-              <h2>Contacts:</h2>
+      <div id="john-container" className="JohnContainer">
+        <h1>Johns Harrison</h1>
+        <h3>"Professional Stack Overflow Explorer"</h3>
+        <div>
+          <div className="Container">
+            <h2>Contacts:</h2>
               <a href="https://www.linkedin.com/in/jharrisons/">LinkedIn</a>
               <a href="https://github.com/JohnsHarrison">GitHub</a>
               <a href="mailto:jshar15089@gmail.com">Email</a>
-            </div>
-            <div className="Container">
-              <h2>Tech:</h2>
-              <p>Javascript</p>
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>React</p>
-            </div>
-            
-          
-
-            </div>
-
-          <button onClick={()=>{
-            handelCLick()}}>Click to be spooked</button>
-          
-
+          </div>
+          <div className="Container">
+            <h2>Tech:</h2>
+            <p>Javascript</p>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>React</p>
+          </div>
         </div>
-      
+        <button onClick={() => handelCLick()}>Click to be spooked</button>
+      </div>
     )
 }
 

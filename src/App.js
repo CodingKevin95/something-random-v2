db1846ffe117aa1367ae56de4dd0a301fd80810c
JohnsHarrison <jshar15089@gmail.com>
2022-08-02 16:36:48 -0400

    rough page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Dominik from "./pages/Dominik";
 import Linfeng from "./pages/Linfeng"
 import Kevin from "./pages/Kevin"
+import John from "./pages/John";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/linfeng" element={<Linfeng /> } />
         <Route path="/dominik" element={<Dominik />} />
         <Route path="/kevin" element={<Kevin />} />
+        <Route path ="/john" element={<John />}/>
       </Routes>
 
     </div>

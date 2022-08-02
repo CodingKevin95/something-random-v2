898c3b64c07b451a58b85904574c6ba42ecaffc7
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-08-02 13:58:23 -0400

    finished page for dominik #2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Dominik from "./pages/Dominik";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/dominik" element={<Dominik />} />
+      </Routes>
     </div>
   );
 }
